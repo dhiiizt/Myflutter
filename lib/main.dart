c31ad9eaac746e9c8af494bba90345adf99b3892
dhiiizt <72489901+dhiiizt@users.noreply.github.com>
2025-10-07 15:36:08 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,34 +10,362 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final colorScheme = ColorScheme(
+      brightness: Brightness.light,
+      primary: const Color(0xFFFF4E4E), // 🔴 Warna utama
+      onPrimary: Colors.white,
+      secondary: const Color(0xFFFF7373),
+      onSecondary: Colors.white,
+      surface: Colors.white,
+      onSurface: const Color(0xFF222222),
+      surfaceContainerHighest: const Color(0xFFFFFFFF),
+      background: const Color(0xFFFFF5F5), // 🎀 Background lembut
+      onBackground: const Color(0xFF444444),
+      error: Colors.red.shade400,
+      onError: Colors.white,
+      primaryContainer: const Color(0xFFFFE9E9),
+      onPrimaryContainer: const Color(0xFF222222),
+    );
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Neru Injector',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: colorScheme,
+        scaffoldBackgroundColor: colorScheme.background,
+        appBarTheme: AppBarTheme(
+          backgroundColor: colorScheme.primaryContainer,
+          foregroundColor: colorScheme.onBackground,
+          elevation: 0,
+        ),
+        cardColor: colorScheme.surface,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: colorScheme.primary,
+            foregroundColor: colorScheme.onPrimary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            elevation: 4,
+          ),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF222222),
+          ),
+          bodyMedium: TextStyle(
+            color: Color(0xFF555555),
+          ),
+        ),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const CollapsiblePage(),
     );
   }
 }
 
+class CollapsiblePage extends StatelessWidget {
+  const CollapsiblePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> sliderImages = [
+      'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg',
+      'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Alpha_(Revenant_of_Roses).jpg',
+      'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Alucard_(Obsidian_Blade).jpg',
+    ];
+
+    final List<Map<String, String>> features = [
+      {
+        'title': 'Unlock All Skins',
+        'subtitle': 'All skin role unlock',
+        'icon':
+            'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg'
+      },
+      {
+        'title': 'Unlock Emotes',
+        'subtitle': '33 Available Emotes',
+        'icon':
+            'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg'
+      },
+      {
+        'title': 'Unlock Recalls',
+        'subtitle': '25 Available Recalls',
+        'icon':
+            'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg'
+      },
+      {
+        'title': 'Drone View',
+        'subtitle': 'Vertical/Horizontal Available',
+        'icon':
+            'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg'
+      },
+      {
+        'title': 'Configuration',
+        'subtitle': 'Games mode api',
+        'icon':
+            'https://raw.githubusercontent.com/dhiiizt/dhiiizt/refs/heads/main/Fighter/Aldous_(Mistbender_Aldous).jpg'
+      },
+    ];
+
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      backgroundColor: colorScheme.background,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: colorScheme.primary),
+              child: const Text(
+                'Neru Tools Menu',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () => Navigator.pop(context),
+            ),
+            const ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.info_outline),
+              title: Text('About'),
+            ),
+          ],
+        ),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          // 🔹 AppBar Collapsible
+          SliverAppBar(
+            expandedHeight: 200.0,
+            pinned: true,
+            backgroundColor: colorScheme.primaryContainer,
+            leading: Builder(
+              builder: (context) => IconButton(
+                icon: Icon(Icons.menu, color: colorScheme.onBackground),
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.shopping_bag_outlined,
+                    color: colorScheme.primary),
+                onPressed: () => ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Cart opened')),
+                ),
+              ),
+              PopupMenuButton<String>(
+                icon: Icon(Icons.more_vert, color: colorScheme.onBackground),
+                onSelected: (value) {
+                  if (value == 'permission') {
+                    showCupertinoModalPopup(
+                      context: context,
+                      builder: (context) => CupertinoActionSheet(
+                        title: Text(
+                          'Permission',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: colorScheme.primary,
+                          ),
+                        ),
+                        message: const Text(
+                          'Pilih satu izin yang ingin digunakan',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black54,
+                          ),
+                        ),
+                        actions: [
+                          CupertinoActionSheetAction(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Default permission dipilih')),
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.settings,
+                                    size: 20, color: colorScheme.primary),
+                                const SizedBox(width: 8),
+                                Text(
+                                  'Default Permission',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: colorScheme.primary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          CupertinoActionSheetAction(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Shizuku permission dipilih')),
+                              );
+                            },
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.shield,
+                                    size: 20, color: colorScheme.primary),
+                                const SizedBox(width: 8),
+                                Text(
+                                  'Shizuku Permission',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: colorScheme.primary,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                        cancelButton: CupertinoActionSheetAction(
+                          isDefaultAction: true,
+                          onPressed: () => Navigator.pop(context),
+                          child: Text(
+                            'Close',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  } else if (value == 'about') {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('App by EsaNeru')),
+                    );
+                  } else if (value == 'exit') {
+                    SystemNavigator.pop();
+                  }
+                },
+                itemBuilder: (context) => const [
+                  PopupMenuItem(value: 'permission', child: Text('Permission')),
+                  PopupMenuItem(value: 'about', child: Text('About')),
+                  PopupMenuItem(value: 'exit', child: Text('Exit')),
+                ],
+              ),
+            ],
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'NERU Injector',
+                style: TextStyle(
+                  color: colorScheme.onSurface,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              background: Image.network(
+                sliderImages[0],
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          // 🔹 Carousel Slider
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 180.0,
+                  autoPlay: true,
+                  enlargeCenterPage: true,
+                  viewportFraction: 0.9,
+                  aspectRatio: 16 / 9,
+                  autoPlayInterval: const Duration(seconds: 4),
+                ),
+                items: sliderImages.map((item) {
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(16),
+                    child: Image.network(
+                      item,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ),
+
+          // 🔹 List Fitur
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                final feature = features[index];
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
+                  child: Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    color: colorScheme.surface,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(feature['icon']!),
+                        radius: 26,
+                      ),
+                      title: Text(
+                        feature['title']!,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: colorScheme.onSurface,
+                        ),
+                      ),
+                      subtitle: Text(
+                        feature['subtitle']!,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: colorScheme.onBackground.withOpacity(0.7),
+                        ),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              backgroundColor: colorScheme.primary,
+                              content: Text(
+                                'Opening ${feature['title']}...',
+                                style: TextStyle(color: colorScheme.onPrimary),
+                              ),
+                            ),
+                          );
+                        },
+                        child: const Text('OPEN'),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              childCount: features.length,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
