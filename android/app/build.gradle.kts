b1c2d27218d0fdca8f19c7abfecb8a4ccc6dbb77
dhiiizt <dhiizthand69@gmail.com>
2025-11-30 10:39:29 +0700

    Prepare to overwrite remote with local meta-new

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -31,6 +31,33 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+    
+    val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+android {
+    ...
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"]!!)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            isShrinkResources = false
+        }
+    }
+}
 
     buildTypes {
         getByName("release") {
